4b31dd55ac4bc2dc17e0ba246eb023d788330048
Diego <millad1@hawkmail.newpaltz.edu>
2018-12-04 23:25:33 -0500

    ui update to exercise page, almost complete

--- src/views/Exercises.vue
+++ src/views/Exercises.vue
@@ -4,9 +4,9 @@
     Choose from our huge selection of exercises!
     <h6>(click the add button next to the exercise you would like to add to your workout)</h6>
   </h1>
-  <h4 class="card-header">
+  <h3 class="card-header">
     Core Exercises
-  </h4>
+  </h3>
   <h6 class="card-body">
     Crunch
     <a href="#" class="btn btn-primary btn-sm active" role="button" aria-pressed="true">+</a>
@@ -27,11 +27,56 @@
     Raised Knee-In
     <a href="#" class="btn btn-primary btn-sm active" role="button" aria-pressed="true">+</a>
   </h6>
-  <h4 class="card-header">
-    Leg Exercises
-  </h4>
+  <div class="card-header">
+    <h3>
+      Leg Exercises
+    </h3>
+    <h8>(some exercises marked with an * requires special equipment)</h8>
+  </div>
   <h6 class="card-body">
     Raised Knee-In
+    <a href="#" class="btn btn-primary btn-sm active" role="button" aria-pressed="true">+</a>
+  </h6>
+  <h6 class="card-body">
+    Squat
+    <a href="#" class="btn btn-primary btn-sm active" role="button" aria-pressed="true">+</a>
+  </h6>
+  <h6 class="card-body">
+    Lunge
+    <a href="#" class="btn btn-primary btn-sm active" role="button" aria-pressed="true">+</a>
+  </h6>
+  <h6 class="card-body">
+    Deadlift*
+    <a href="#" class="btn btn-primary btn-sm active" role="button" aria-pressed="true">+</a>
+  </h6>
+  <h6 class="card-body">
+    Leg Press*
+    <a href="#" class="btn btn-primary btn-sm active" role="button" aria-pressed="true">+</a>
+  </h6>
+  <div class="card-header">
+    <h3>
+      Arm Exercises
+    </h3>
+    <h8>(some exercises marked with an * requires special equipment)</h8>
+  </div>
+  <h6 class="card-body">
+    Push-up
+    <a href="#" class="btn btn-primary btn-sm active" role="button" aria-pressed="true">+</a>
+  </h6>
+  <h6 class="card-body">
+    Pullover/Tricep Extension*
+    <a href="#" class="btn btn-primary btn-sm active" role="button" aria-pressed="true">+</a>
+  </h6>
+  <h6 class="card-body">
+    Bicep Curl*
+    <a href="#" class="btn btn-primary btn-sm active" role="button" aria-pressed="true">+</a>
+  </h6>
+  <h6 class="card-body">
+    Chin-up*
+    <a href="#" class="btn btn-primary btn-sm active" role="button" aria-pressed="true">+</a>
+  </h6>
+  <h6 class="card-body">
+    Pull-up*
     <a href="#" class="btn btn-primary btn-sm active" role="button" aria-pressed="true">+</a>
   </h6>
 </div>
